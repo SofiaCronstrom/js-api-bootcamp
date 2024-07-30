e5238b424425cbf1f5b6953d09b0bbff9633477c
Sofia <sofia.cronstrom@gmail.com>
2024-07-30 14:34:41 +0200

    exercise 2 part 1 and 2

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-fetchPokemon();
+/////Exercise 1
 
-function fetchPokemon (){
-    fetch('https://santosnr6.github.io/Data/pokemons.json')
-    .then((response) => response.json())
-    .then((data) => {
-       data.forEach(obj => {
-        document.body.innerHTML += `<img src='${obj.image}' /> ${obj.name} </br>`;
-       })
-    })
-    .catch(error => console.log(error));
+// fetchPokemon();
 
-    
+// function fetchPokemon() {
+//   fetch("https://santosnr6.github.io/Data/pokemons.json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.forEach((obj) => {
+//         document.body.innerHTML += `<img src='${obj.image}' /> ${obj.name} </br>`;
+//       });
+//     })
+//     .catch((error) => console.log(error));
+// }
+
+//Exercise 2
+renderDogs();
+
+async function renderDogs(){
+    let dogs = await fetchDogs();
+    dogs.forEach(dog => {
+        console.log(dog);
+    });
+
+
+}
+
+async function fetchDogs(){
+    try {
+    let response = await fetch('https://majazocom.github.io/Data/dogs.json');
+    let data = await response.json();
+    return data;
+    } catch(error){
+        console.log(error);
+    }
 }
