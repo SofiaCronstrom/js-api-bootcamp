a62b8408f979a91f3dd182fa52375cd9d315abfb
Sofia <sofia.cronstrom@gmail.com>
2024-07-30 13:40:29 +0200

    exercise 1 part 3

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log(fetchPokemon())
+fetchPokemon();
 
 function fetchPokemon (){
     fetch('https://santosnr6.github.io/Data/pokemons.json')
     .then((response) => response.json())
     .then((data) => {
        data.forEach(obj => {
-        console.log(obj);
+        document.body.innerHTML += `<img src='${obj.image}' /> ${obj.name} </br>`;
        })
     })
     .catch(error => console.log(error));
